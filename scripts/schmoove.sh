31390fa0e478a5aec0cf3db938e9c2c74b78bde3
mantacid <mantacid@protonmail.com>
2023-08-17 22:25:33 -0700

    tidied up schmoovement system, drafted documentation in source comments.

--- scripts/schmoove.sh
+++ scripts/schmoove.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
-## handles the schmovement of data
+## This file handles the Schmoovement of data between tabs, windows, and other bingle elements. It's currently a bit hacky, but I hope to polish it up incrementally.
+
 ## NOTE: make a directory for each window, so that its window definition can be stored and accessed by eww.
 
-##define variables global to this scripts
-WIN_BASE="(defwindow WINDOW :geometry (geometry :anchor 'top left') :wm-ingore false :stacking 'bg' :windowtype 'normal' (winBingle :content-yuck '(context)' :winUUID 'WINDOW'))"
+#######################################################################################
+## get-temp-dir
+## a small function to print the location of the temporary directory in which the working files will be placed.
+## Takes no arguments.
+## Returns the path to the temporary directory.
 
 get-temp-dir() {
   echo $(echo "$(pwd)" | sed 's/scripts/tmp\//g')
 }
-## handler function to open a window
+
+#######################################################################################
+## tab-add: handler function to create tabs with UUIDs and associate them to windows.
+## Takes in a window UUID, the yuck associated with the tab, the label to give the tab, and the icon to show on the tab, if the styles permit it.
+## Returns the tab's UUID
+## WIP!
+
+tab-add() {
+  ## define local variables
+  baseJSON="TABUUID: {name: 'LABEL', icon: 'ICON', content: 'CONTENT'},"
+  tabUUID=$(uuidgen)
+
+  ## define names for arguments
+  winUUID="$1"
+  appYuck="$2"
+  tabLabl="$3"
+  tabIcon="$4"
+
+
+}
+
+#######################################################################################
+## window-open: A handler function to open windows & associate UUIDs with them.
+## Takes in first tab as arguments, as well as window position and size.
+## Returns the window UUID
+## WIP!
+
 window-open() {
   ## define local variables
+  winBASE="(defwindow WINDOW :geometry (geometry :anchor 'top left') :wm-ingore false :stacking 'bg' :windowtype 'normal' (winBingle :content-yuck '(context)' :winUUID 'WINDOW'))"
   winUUID=$(uuidgen)
 
   ## define names for arguments
@@ -20,10 +51,12 @@ window-open() {
   winW="$4"
   winH="$5"
 
-  ## format window position for use as an argument to `eww open`
+  ## format window position and size for use as an argument to `eww open`
+  winPos="${winX}x${winY}"
+  winSiz="${winW}x${winH}"
 
   ## replace all instances of placeholder text 'WINDOW' with the window's UUID, so that it can be referred to and passed to children widgets
-  winYuck=$(echo $WIN_BASE | sed "s/WINDOW/${winUUID}/g")
+  winYuck=$(echo $winBASE | sed "s/WINDOW/${winUUID}/g")
 
   ##initialize the temporary directory
   winDir="$(get_temp_dir)/$winUUID"
@@ -36,17 +69,34 @@ window-open() {
   pointerYuck="(include 'tmp/$winUUID/def.yuck')"
   sed -i 'ls/^/${pointerYuck}\n/' $(get-temp-dir)/schmoove-pointers.yuck
 
+  ## open the window, which will contain a variable to track its assigned files
+
   ## handle the tab json using the tab-add function (WIP)
+
+  ## once the tab is created, update the last-focused-tab variable to focus the new tab. This will trigger the content to render in the parent window.
+
+  ## return the new window UUID
+  echo $winUUID
 }
 
-window-close() {
-##WIP
-}
-
-tab-add() {
-##WIP
-}
+#######################################################################################
+## tab-remove: removes a tab from a window file
+## Takes in a window UUID, a tab UUID.
+## Returns nothing.
+## WIP!
 
 tab-remove() {
-##WIP
+
 }
+
+#######################################################################################
+## window-close: a handler function to close windows and remove any temporary resources associated with them
+## Takes in the UUID of the window to close
+## Returns nothing.
+## WIP!
+
+window-close() {
+
+}
+
+
