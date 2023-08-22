ad7d6295b7b36ce0b282ad4227f90f9c2d2dcfcd
mantacid <mantacid@protonmail.com>
2023-08-21 17:41:32 -0700

    removed unneccesary complexity in tab json.

--- scripts/schmoove.sh
+++ scripts/schmoove.sh
@@ -26,7 +26,7 @@ get-temp-dir() {
 
 tab-add() {
   ## define local variables
-  baseJSON="TABUUID: {name: 'LABEL', icon: 'ICON', content: 'CONTENT', winUUID: 'WINUUID'},"
+  baseJSON="TABUUID: {name: 'LABEL', icon: 'ICON', content: 'CONTENT'}"
   tabUUID=$(uuidgen)
 
   ## define names for arguments
@@ -37,7 +37,7 @@ tab-add() {
 
 
   ## generate the tab json. This will be inserted into the window's tab list. tabs appear in an order defined by their index in the list.
-  tabJSON=$(echo $(echo $(echo $(echo $(echo $baseJSON | sed "s/TABUUID/$tabUUID/g") | sed "s/LABEL/$tabLabl/g") | sed "s/ICON/$tabIcon/g") | sed "s/CONTENT/$appYuck/g") | sed "s/WINUUID/$winUUID/g")
+  tabJSON=$(echo $(echo $(echo $(echo $baseJSON | sed "s/TABUUID/$tabUUID/g") | sed "s/LABEL/$tabLabl/g") | sed "s/ICON/$tabIcon/g") | sed "s/CONTENT/$appYuck/g")
 
   ## find the window JSON file, use python implementation to populate the tab array.
 }
