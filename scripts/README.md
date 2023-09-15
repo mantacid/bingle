6a699f87988ae793c3b15ac98c8cf4104623a912
mantacid <mantacid@protonmail.com>
2023-09-15 15:04:33 -0700

    improved the bicon script. Still getting unexplained errors.

--- scripts/README.md
+++ scripts/README.md
@@ -1,6 +1,9 @@
 # `scripts/`
 This directory contains various scripts and binaries used by the system to perform tasks.
 
+## `bicon.py`
+**BI**ngle **CON**figurator. Handles the updating, setting, initialization, and translation of Config JSON into a form that eww can understand (no whitespace outside of strings), while maintaining human readability. **WIP**
+
 ## `buildContextMenu.sh`
 WIP. Will handle the creation of the context menu.
 
@@ -8,5 +11,5 @@ WIP. Will handle the creation of the context menu.
 uses `xdotool` to get the mouse position. Required by the context menu.
 
 ## `schmoove.sh`
-Handler script to facilitate window creation/destruction, as well as tab display, dragging, and data.
+Handler script to facilitate window creation/destruction, as well as tab display, dragging, and data. **WIP**
 
--- scripts/bicon.py
+++ scripts/bicon.py
@@ -6,15 +6,18 @@
 import sys
 import json
 import re
+from pandas.io.json import json_normalize
 
 #################################################################################
 ## format json string to have no whitespace outside of strings.
 def format(JSON_STR):
-  x = json.dumps(JSON_STR, separators=(',', ':'), indent=None)
-  ## regex junk away
-  x = re.sub(r"(\\n)[\s]*(\\?)", "", x)
-  x = re.sub(r"\\", "", x)
-  return x
+  JSON_STR = re.sub(r"\n", "", JSON_STR)
+  JSON_STR = re.sub(r"(?<![\[\"\w\d])\s+", "", JSON_STR)
+  JSON_STR = re.sub(r"\s}", "", JSON_STR)
+
+  #print(JSON_STR)
+  DICT = json.loads(testStr)
+  return DICT
 
 #################################################################################
 ## parses Pretty json file into python dict for easy manipulation. This means we dont get live config updates, but it also means we aren't writing to the disk as much.
@@ -34,16 +37,12 @@ def update(DICT, KEY, VAL):
 
 #################################################################################
 ## internal function. prints full expanded names of keys in dot notation. try to not call this on its own.
-def walk_keys(OBJ, path=""):
-  if isinstance(OBJ, dict):
-    for k, v in obj.iteritems():
-      yield from walk_keys(v, path + "." + k if path else k)
-  elif isinstance(OBJ, list):
-    for i, v in enumerate(OBJ):
-      s = "[" + str(i) + "]"
-      yield from walk_keys(v, path + s if path else k)
-  else:
-    yield path
+def walk_keys(OBJ):
+  ## make OBJ into a python dict. THIS THING IS DRIVING ME INSANE
+  DICT = json.loads(OBJ)
+  #print(DICT)
+  ToC = json_normalize(DICT, sep='.')
+  return ToC
 
 #################################################################################
 ## Print a list of all expanded keys in file at PATH using dot notation, add output to list defined in LIST
@@ -60,15 +59,15 @@ def init(PATH):
   CONF_DICT = parse(PATH)
 
   ## loop through all keys, prompt for input of type defined in key name.
-  ## we don't know how many nests the json will have (most likely its five, but I want to future-proof this), so use recursion to get all the keys.
+  ## we don't know how many nests the json will have (most likely its five, but only if the settings GUI will prohibit user inputs that are dicts. Plus, it would be a good idea for me to future-proof this now.), so use recursion to get all the keys.
 
 #################################################################################
 ## DEBUG CALLS
 A = ['']
 X = "/home/bingle/github/bingle/conf/main.json"
-print(X)
+#print(X)
 Y = parse(X)
-print(Y)
+#print(Y)
 Z = trace(X, A)
-print(Z)
-print(A)
+#print(Z)
+#print(A)
