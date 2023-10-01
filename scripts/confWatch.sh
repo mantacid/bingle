76310b9abc681d695fcc515dee7cdafa059c265c
mantacid <mantacid@protonmail.com>
2023-09-30 18:52:02 -0700

    added a small bash script to allow yuck to listen for changes to the main config.

---
+++ scripts/confWatch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+####################################################################################
+## watch: watches a config at CONF_PATH for changes.
+watch_changes() {
+  ## local names for arguments
+  CONF_PATH="$1"
+  ## watch the config file
+  TEMP_FILE=$(mktemp)
+  (fswatch -bat --event=Updated $CONF_PATH) >> $TEMP_FILE
+  ## prevent the file from growing too large?
+  ## WIP ##
+  ## output the name of the tempfile so that the yuck can create a listener for it
+  echo $TEMP_FILE
+}
+####################################################################################
+## MAIN CALL ##
+watch_changes '/home/bingle/.config/eww/conf/main.json'
